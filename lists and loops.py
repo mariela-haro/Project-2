d2e31efccfcaba499f2920fb486a191d6065dda4
mariela-haro <143922599+mariela-haro@users.noreply.github.com>
2024-10-09 14:03:51 -0700

    Add files via upload

---
+++ lists and loops.py
@@ -0,0 +1,14 @@
+import csv
+
+movies = [
+    ['Barbie', 'Comedy', 78],
+    ['The Lego Movie', 'Animation', 96],
+    ['Twilight', 'Romance', 68],
+    ['9 to 5', 'Comedy', 89],
+    ['Coraline', 'Animation', 80]
+]
+movies.append(['Inside Out', 'Family', 93])
+
+for row in movies:
+    print(row)
+
